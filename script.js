aab551a83fdcaf28ed90c3cbeb4a30164c7ab6ec
Alex <kuanalex96@gmail.com>
2021-06-04 12:58:31 -0400

    adding async/await/ to clean up code

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const img = document.querySelector('img');
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=Cjwa4WlJE3vmLW0SB7VLmHuKyRTsDvsY&s=cats', {
+
+async function getCats() {
+    const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=Cjwa4WlJE3vmLW0SB7VLmHuKyRTsDvsY&s=cats', {
         mode: 'cors'
-    })
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (response) {
-        img.src = response.data.images.original.url;
     });
+    const catData = await response.json();
+    img.src = catData.data.images.original.url;
+}
+
+getCats();
 
 // https://api.giphy.com/v1/gifs/translate?api_key=Cjwa4WlJE3vmLW0SB7VLmHuKyRTsDvsY&s=cats
